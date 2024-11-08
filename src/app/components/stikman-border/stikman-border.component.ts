47513fab2175833960c3131ec3a1fbd881b6be77
Darren Bullock <darren.bullock@gppos.net>
2024-11-08 14:10:42 +0200

    scale images

--- src/app/components/stikman-border/stikman-border.component.ts
+++ src/app/components/stikman-border/stikman-border.component.ts
@@ -16,10 +16,15 @@ export class StikmanBorderComponent {
   }
 
   stikimageArray: string[] = [
+    'https://firebasestorage.googleapis.com/v0/b/st1kmanonsol.appspot.com/o/images%2Fst1kman-variations%2Fhands-up.webp?alt=media&token=66d5e8e5-e5bf-4aff-8cd2-48c662124654',
+    'https://firebasestorage.googleapis.com/v0/b/st1kmanonsol.appspot.com/o/images%2Fst1kman-variations%2Fhard-walk.webp?alt=media&token=16eee743-7eb7-47e8-ad7c-73ea464efc80',
+    'https://firebasestorage.googleapis.com/v0/b/st1kmanonsol.appspot.com/o/images%2Fst1kman-variations%2Fjump-funny-face.webp?alt=media&token=343e3cf2-56fc-45e2-9e8a-4f027d4d4a1b',
+    'https://firebasestorage.googleapis.com/v0/b/st1kmanonsol.appspot.com/o/images%2Fst1kman-variations%2Fjump-point.webp?alt=media&token=108a6842-8279-4cb0-91ca-251a09c63098',
+    'https://firebasestorage.googleapis.com/v0/b/st1kmanonsol.appspot.com/o/images%2Fst1kman-variations%2Frunning.webp?alt=media&token=8ae9bdcf-5a56-4329-a8c4-61329ccc405c',
+    'https://firebasestorage.googleapis.com/v0/b/st1kmanonsol.appspot.com/o/images%2Fst1kman-variations%2Fshow-hand.webp?alt=media&token=fc150362-b6cf-41ff-ab29-fc7a450acc4b',
+    'https://firebasestorage.googleapis.com/v0/b/st1kmanonsol.appspot.com/o/images%2Fst1kman-variations%2Fsmall-fingers.webp?alt=media&token=b7ca1e8e-4797-47e1-bf55-6ee453f09afe',
+    'https://firebasestorage.googleapis.com/v0/b/st1kmanonsol.appspot.com/o/images%2Fst1kman-variations%2Fstand-smile.webp?alt=media&token=35657e6e-c627-42be-bce9-9bc5bfd96301',
     'https://firebasestorage.googleapis.com/v0/b/st1kmanonsol.appspot.com/o/images%2Fst1kman-variations%2Fwhat%20anime%20surprise.png?alt=media&token=656de241-9f7c-4bd9-9376-39b91c4d1906',
-    'https://firebasestorage.googleapis.com/v0/b/st1kmanonsol.appspot.com/o/images%2Fst1kman-variations%2Fdirect_point_grimace.png?alt=media&token=c52fd67c-44c3-4c41-8344-a27a2939c8c2',
-    'https://firebasestorage.googleapis.com/v0/b/st1kmanonsol.appspot.com/o/images%2Fst1kman-variations%2Fhand_out_happy_.png?alt=media&token=6146eb7a-9fd8-4334-a14a-8d6bb98e9b58',
-    'https://firebasestorage.googleapis.com/v0/b/st1kmanonsol.appspot.com/o/images%2Fst1kman-variations%2Fhey%20jump.png?alt=media&token=06a25e60-ba90-4048-9271-bb0048719dca',
   ];
   // input for horizontal
   @Input() set horizontalInput(value: boolean) {
